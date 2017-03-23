392730b2dd5d6af3c20e954bc2ee65b6a4476d7e
Fogolan <Fogolan@gmail.com>
2017-03-23 23:47:53 +0300

    started adding user geolocation

---
+++ Models/AppDbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebstackOfLove.Properties;
+
+namespace WebstackOfLove.Models
+{
+    public class AppDbInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            User test = new User
+            {
+                Id = "gsdfgsdfgsdfgasfdg",
+                UserName = "TestUser",
+                SecretKey = Settings.Default.SecretKey
+            };
+            context.Users.Add(test);
+            context.SaveChanges();
+            base.Seed(context);
+        }
+
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -7,6 +7,9 @@ namespace WebstackOfLove.Models
 {
     public class User
     {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string SecretKey { get; set; }
 
     }
 }
